0633e3a2de698ba30d036067e20af28d1dd54be5
brav3luk3 <oleksandrovich2000@gmail.com>
2023-05-25 21:11:59 +0300

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-test
+# Time comparison of data structures
+
+## Description
+The program was created to view the running time of Arraylist, Linkedlist, HashSet,
+TreeSet, HashMap and TreeMap depending on the number of elements added,
+removed and retrieved.
+
+Also, the purpose of creating the program was to gain knowledge for working with the
+above data structures.
+
+## Main classes
+1. Person
+2. AbstractHandler
+3. ListHandler
+4. MapHandler
+5. SetHandler
+6. RandomUtils
+
+## Description of program classes
+
+Person - stores information about a person, such as: name, age, id.
+
+AbstractHandler - this is an abstract class that contains methods such as:
+* add(int amountElements) - to add the specified number of elements
+* remove(int amountElements) - to remove the specified number of elements
+* get(int amountElements) - to get the specified number of elements
+
+ListHandler - a class that extends by AbstractHandler and uses its methods to
+adding, removing and getting elements.
+
+MapHandler - a class that extends by AbstractHandler and uses its methods to
+adding, removing and getting elements.
+
+SetHandler - a class that contains methods like:
+* add() - to add elements
+* remove(int amountElements) - to remove the specified number of elements
+* get(int amountElements) - to get the specified number of elements
+
+RandomUtils - a class that contains helper methods, such as:
+* randomNumber(int max) - to generate a random number from 0 to the specified
+* randomChar() - to generate a random letter
+* getRandomElements(int amountElements, Collection<Person> collection) - for sampling
+the specified number of random elements from the collection
+
+## Program example
+
+![program execution example](readme_img/1.png)
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,47 +5,49 @@ public class Main {
     public static void main(String[] args) {
 
         int elements = 1_000_000;
+        int amountElementsToRemove = 8000;
+        int amountElementsToGet = 8000;
 
         System.out.println("\n////////////ArrayLIst////////////\n");
 
         ListHandler listHandler = new ListHandler(elements, true);
-        listHandler.add();
-        listHandler.remove(500);
-        listHandler.get(8000);
+        listHandler.add(elements);
+        listHandler.remove(amountElementsToRemove);
+        listHandler.get(amountElementsToGet);
 
         System.out.println("\n///////////LinkedList////////////\n");
 
         listHandler = new ListHandler(elements, false);
-        listHandler.add();
-        listHandler.remove(500);
-        listHandler.get(8000);
+        listHandler.add(elements);
+        listHandler.remove(amountElementsToRemove);
+        listHandler.get(amountElementsToGet);
 
         System.out.println("\n//////////////HashSet////////////\n");
 
         SetHandler setHandler = new SetHandler(elements, true);
         setHandler.add();
-        setHandler.remove(500);
-        setHandler.get(8000);
+        setHandler.remove(amountElementsToRemove);
+        setHandler.get(amountElementsToGet);
 
         System.out.println("\n/////////////TreeSet/////////////\n");
 
         setHandler = new SetHandler(elements, false);
         setHandler.add();
-        setHandler.remove(500);
-        setHandler.get(8000);
+        setHandler.remove(amountElementsToRemove);
+        setHandler.get(amountElementsToGet);
 
         System.out.println("\n/////////////HashMap//////////////\n");
 
         MapHandler mapHandler = new MapHandler(elements, true);
-        mapHandler.add();
-        mapHandler.remove(8000);
-        mapHandler.get(8000);
+        mapHandler.add(elements);
+        mapHandler.remove(amountElementsToRemove);
+        mapHandler.get(amountElementsToGet);
 
         System.out.println("\n/////////////TreeMap//////////////\n");
 
         mapHandler = new MapHandler(elements, false);
-        mapHandler.add();
-        mapHandler.remove(8000);
-        mapHandler.get(8000);
+        mapHandler.add(elements);
+        mapHandler.remove(amountElementsToRemove);
+        mapHandler.get(amountElementsToGet);
     }
 }
